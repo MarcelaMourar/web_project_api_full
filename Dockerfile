2fb385e62473dfc0408d8412d937d38dd597e961
Marcela Moura <marcela_moura28@hotmail.com>
2026-02-18 21:19:14 +0100

    salvando as alteraçoes

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,16 @@
 FROM node:18-slim
+
+# Define o diretório de trabalho dentro do container
 WORKDIR /app
-COPY package*.json ./
+
+# Copia os arquivos de configuração que estão DENTRO da pasta backend
+COPY backend/package*.json ./
+
+# Instala as dependências (estando agora dentro de /app)
 RUN npm install --only=production
-# Isso copia a pasta backend e o app.js para dentro do container
-COPY . .
-ENV PORT 8080
+
+# Copia todo o conteúdo da pasta backend para a raiz /app do container
+COPY backend/ .
+
+# O comando agora aponta para o app.js que foi copiado para a raiz do container
 CMD ["node", "app.js"]
--- backend/app.js
+++ backend/app.js
@@ -3,17 +3,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require ('cors');
 const { errors: celebrateErrors } = require('celebrate');
-const auth = require('./backend/middleware/auth');
-const { requestLogger, errorLogger} = require ('./backend/middleware/logger');
-const errorHandler = require ('./backend/middleware/errorHandler');
-const { validateSignup, validateSignin} = require ('./backend/middleware/validation');
-const {createUser, login} = require ('./backend/controllers/users');
-const usersRouter = require ('./backend/routes/users');
-const cardsRouter = require('./backend/routes/cards');
+const auth = require('./middleware/auth');
+const { requestLogger, errorLogger} = require ('./middleware/logger');
+const errorHandler = require ('./middleware/errorHandler');
+const { validateSignup, validateSignin} = require ('./middleware/validation');
+const {createUser, login} = require ('./controllers/users');
+const usersRouter = require ('./routes/users');
+const cardsRouter = require('./routes/cards');
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 const MONGO_URL = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/aroundb';
 
+mongoose.connect(MONGO_URL)
+  .then(() => console.log('Conectado ao MongoDB com sucesso!'))
+  .catch((err) => console.log('Erro ao conectar ao MongoDB:', err));
 
 app.use(cors());
 app.use(express.json());
